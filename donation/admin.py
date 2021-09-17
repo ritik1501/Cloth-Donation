2799f1843af3becacfa67c488dea2a8eb28d548b
ritik <ritik-01>
2021-09-17 22:49:41 +0530

    Initial Commit

---
+++ donation/admin.py
@@ -0,0 +1,6 @@
+from donation.models import Clothes, Person
+from django.contrib import admin
+
+# Register your models here.
+admin.site.register(Person)
+admin.site.register(Clothes)
---
+++ donation/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name            = models.CharField(max_length=50)
+    email           = models.EmailField(max_length=100, blank=True, null=True)
+    phone_number    = models.CharField(max_length=10, blank=True, null=True)
+    address         = models.JSONField()
+
+    def __str__(self):
+        return self.name + " - " + self.email
+
+    class Meta:
+        verbose_name_plural = "Person"
+        
+
+
+class Clothes(models.Model):
+    no_clothes      = models.IntegerField()
+    cloth_type      = models.CharField(max_length=500)
+    cloth_gender    = models.CharField(max_length=20)
+    timestamp       = models.DateTimeField(auto_now_add=True) 
+    person          = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.person.name + " - " + self.cloth_gender
+
+    class Meta:
+        verbose_name_plural = "Clothes"
